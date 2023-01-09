b0ad893638c1f2e45387359ffd6c4df4df7ac31e
Luis Utrera <lutrera@simtlix.com>
2023-01-09 09:08:25 -0400

    initial commit. Added models and still building chat_repository

--- lib/shared/domain/chat_repository/chat_repository.dart
+++ lib/shared/domain/chat_repository/chat_repository.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_chat/participant/domain/basic_chat_participant.dart';
+import 'package:firebase_chat/room/domain/room.dart';
 import 'package:firebase_chat/shared/domain/chat_repository/room_creation_failure.dart';
-import 'package:firebase_chat/shared/domain/core/result_or.dart';
+import 'package:firebase_chat/shared/domain/core/core.dart';
 
 abstract class ChatRepository {
-  Future<ResultOr<RoomCreationFailure>> createRoom(
+  Future<Resource<RoomCreationFailure, ChatRoom>> lazyCreateRoom(
+    Set<BasicChatParticipant> chatParticipants,
+  );
+  Future<Resource<RoomCreationFailure, ChatRoom>> createRoom(
     Set<BasicChatParticipant> chatParticipants,
   );
 }
